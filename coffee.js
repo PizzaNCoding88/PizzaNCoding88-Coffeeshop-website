49f3e8e4c602fbf6629fcdf9924ee3047d38696e
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-01-11 09:42:35 +0000

    overlay added - side menu

--- coffee.js
+++ coffee.js
@@ -1,19 +1,18 @@
 const closingButton = document.getElementById("navigation-menu__closing-button");
 const sideMenu = document.querySelector(".navigation-menu");
 const burgerMenu = document.querySelector(".burger-menu");
+const overlay = document.querySelector(".overlay");
 
-burgerMenu.addEventListener("click", () => {
+burgerMenu.addEventListener("click", overlaySideMenu);
+closingButton.addEventListener("click", overlaySideMenu);
+overlay.addEventListener("click", overlaySideMenu);
+
+function overlaySideMenu(){
     if(!sideMenu.classList.contains("open")){
         sideMenu.classList.add("open");
+        overlay.classList.add("overlay-active");
     } else{
         sideMenu.classList.remove("open");
+        overlay.classList.remove("overlay-active");
     }
-})
-
-closingButton.addEventListener("click", () => {
-    if(!sideMenu.classList.contains("open")){
-        sideMenu.classList.add("open");
-    } else{
-        sideMenu.classList.remove("open");
-    }
-})
+}
